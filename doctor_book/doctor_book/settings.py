f5dfddd54cf5fca081d0c82d4c1b511c73bbc315
Sayan Munsi <sayanmunsi8@gmail.com>
2026-02-18 18:11:37 +0530

    Replace email credentials with placeholders
    Updated email configuration to use placeholder values.

--- doctor_book/doctor_book/settings.py
+++ doctor_book/doctor_book/settings.py
@@ -130,8 +130,8 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
-EMAIL_HOST_USER='isayan635@gmail.com'
-EMAIL_HOST_PASSWORD='rxpmoxlsisuphcvo'
+EMAIL_HOST_USER='Your Mail ID'
+EMAIL_HOST_PASSWORD='Password'
 
 REST_FRAMEWORK = {
 
